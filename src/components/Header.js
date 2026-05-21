4de921ea1216b501acc8e842672c4507530ccbc0
Fejiro001 <aberefejiro@gmail.com>
2026-05-21 13:33:22 -0500

    Styled the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,19 @@ import logo from "../media/logo.svg";
 function Header() {
   return (
     <header>
-      <div className="logo">
+      <div className="container header-container">
         <img
           className="logo-image"
           src={logo}
           alt="My Tasks application logo"
         />
-        <h1 className="app-title">My Tasks</h1>
-        <div className="task-count">
-          <p>10 pending</p>
-          <span>•</span>
-          <p>5 done</p>
+        <div className="header-content">
+          <h1 className="app-title">My Tasks</h1>
+          <div className="task-count">
+            <p>10 pending</p>
+            <span>•</span>
+            <p>5 done</p>
+          </div>
         </div>
       </div>
     </header>
