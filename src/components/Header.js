c8eade90137b82d09dd2a5da9cae0aa3f1af8422
Fejiro001 <aberefejiro@gmail.com>
2026-05-21 09:47:21 -0500

    Added task count

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,17 @@ function Header() {
   return (
     <header>
       <div className="logo">
-        <img className="logo-image" src={logo} alt="My Tasks application logo" />
+        <img
+          className="logo-image"
+          src={logo}
+          alt="My Tasks application logo"
+        />
         <h1 className="app-title">My Tasks</h1>
+        <div className="task-count">
+          <p>10 pending</p>
+          <span>•</span>
+          <p>5 done</p>
+        </div>
       </div>
     </header>
   );
