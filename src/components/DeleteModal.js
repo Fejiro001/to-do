7cfe1f223179550472f01aac4492358aa3d5169c
Fejiro001 <aberefejiro@gmail.com>
2026-05-22 16:16:02 -0500

    Added a task deletion modal

---
+++ src/components/DeleteModal.js
@@ -0,0 +1,49 @@
+function DeleteModal(props) {
+  const {
+    showModal,
+    setShowModal,
+    handleDelete,
+    doNotShowAgain,
+    setDoNotShowAgain
+  } = props;
+
+  return (
+    <>
+      {showModal && doNotShowAgain !== "true" && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>Are you sure you want to delete this task?</h2>
+            <div className="confirmation-option">
+              <input
+                type="checkbox"
+                id="do-not-show"
+                name="do-not-show"
+                checked={doNotShowAgain}
+                onChange={(e) => {
+                  setDoNotShowAgain(e.target.checked);
+                  sessionStorage.setItem(
+                    "doNotShowModal",
+                    e.target.checked.toString()
+                  );
+                }}
+              />
+              <label htmlFor="do-not-show">Don't show this again</label>
+            </div>
+            <div className="delete-actions">
+              <button onClick={handleDelete} className="confirm-delete">
+                Yes, Delete
+              </button>
+              <button
+                onClick={() => setShowModal(false)}
+                className="cancel-delete">
+                No, Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default DeleteModal;
--- src/components/Task.js
+++ src/components/Task.js
@@ -6,11 +6,16 @@ import {
   FaCheck,
   FaX
 } from "react-icons/fa6";
+import DeleteModal from "./DeleteModal";
 
 function Task(props) {
   const { task, updateTask, completeTask, deleteTask } = props;
   const [isEditing, setEditing] = useState(false);
   const [editText, setEditText] = useState(task.text);
+  const [showModal, setShowModal] = useState(false);
+  const [doNotShowAgain, setDoNotShowAgain] = useState(
+    sessionStorage.getItem("doNotShowModal") === "true"
+  );
   const textRef = useRef(null);
 
   const handleTextEdit = () => {
@@ -29,67 +34,87 @@ function Task(props) {
     }
   }, [isEditing]);
 
+  const handleShowModal = () => {
+    console.log(doNotShowAgain);
+    setShowModal(true);
+  };
+
+  const handleDelete = () => {
+    deleteTask(task);
+    setShowModal(false);
+  };
+
   return (
-    <div
-      className={`task-list-item ${task.isCompleted ? "task-completed" : ""}`}>
-      {!isEditing ? (
-        <>
-          <h2 className="task-title">{task.text}</h2>
-          <p className="task-date">{task.date}</p>
-          <hr />
-          <ul className="task-actions">
-            <li title="Complete Task">
-              <button
-                onClick={() =>
-                  completeTask({ ...task, isCompleted: !task.isCompleted })
-                }
-                className="complete">
-                <FaCircleCheck />
-              </button>
-            </li>
-            <li title="Edit Task">
-              <button onClick={handleTextEdit} className="edit">
-                <FaPencil />
-              </button>
-            </li>
-            <li title="Delete Task">
-              <button onClick={() => deleteTask(task)} className="delete">
-                <FaTrash />
-              </button>
-            </li>
-          </ul>
-        </>
-      ) : (
-        <>
-          <form
-            className="edit-task-form"
-            onSubmit={(e) => {
-              updateTask(e, { ...task, text: editText });
-              setEditing(false);
-            }}
-            onBlur={() => setEditing(false)}>
-            <textarea
-              value={editText}
-              ref={textRef}
-              name="editedTask"
-              // rows={3}
-              onChange={(e) => setEditText(e.target.value)}></textarea>
-            <div className="edit-form-actions">
-              <button title="Save Edit" className="save-edit" type="submit">
-                <FaCheck />
-              </button>
-              <button
-                title="Close Edit"
-                onClick={() => setEditing(false)}
-                className="close-edit"
-                type="button">
-                <FaX />
-              </button>
-            </div>
-          </form>
-        </>
-      )}
-    </div>
+    <>
+      <DeleteModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        doNotShowAgain={doNotShowAgain}
+        setDoNotShowAgain={setDoNotShowAgain}
+        handleDelete={handleDelete}
+      />
+      <div
+        className={`task-list-item ${task.isCompleted ? "task-completed" : ""}`}>
+        {!isEditing ? (
+          <>
+            <h2 className="task-title">{task.text}</h2>
+            <p className="task-date">{task.date}</p>
+            <hr />
+            <ul className="task-actions">
+              <li title="Complete Task">
+                <button
+                  onClick={() =>
+                    completeTask({ ...task, isCompleted: !task.isCompleted })
+                  }
+                  className="complete">
+                  <FaCircleCheck />
+                </button>
+              </li>
+              <li title="Edit Task">
+                <button onClick={handleTextEdit} className="edit">
+                  <FaPencil />
+                </button>
+              </li>
+              <li title="Delete Task">
+                <button
+                  onClick={doNotShowAgain ? handleDelete : handleShowModal}
+                  className="delete">
+                  <FaTrash />
+                </button>
+              </li>
+            </ul>
+          </>
+        ) : (
+          <>
+            <form
+              className="edit-task-form"
+              onSubmit={(e) => {
+                updateTask(e, { ...task, text: editText });
+                setEditing(false);
+              }}
+              onBlur={() => setEditing(false)}>
+              <textarea
+                value={editText}
+                ref={textRef}
+                name="editedTask"
+                onChange={(e) => setEditText(e.target.value)}></textarea>
+              <div className="edit-form-actions">
+                <button title="Save Edit" className="save-edit" type="submit">
+                  <FaCheck />
+                </button>
+                <button
+                  title="Close Edit"
+                  onClick={() => setEditing(false)}
+                  className="close-edit"
+                  type="button">
+                  <FaX />
+                </button>
+              </div>
+            </form>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
